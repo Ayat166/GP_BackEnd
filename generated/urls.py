638add8560e897213cbb5fc3ef07254b35180a53
Ayat166 <ayatali1661@gmail.com>
2024-05-07 20:57:00 +0300

    Add Fav generated image and Delete it

--- generated/urls.py
+++ generated/urls.py
@@ -3,4 +3,6 @@ from .views import UserGeneratedView
 
 urlpatterns = [
     path('user-generated-image/', UserGeneratedView.as_view(), name='user_generated_image'),
+    path('user-generated-image/<int:FavId>/', UserGeneratedView.as_view(), name='user_generated_image_Fav'),
+    path('user-generated-image/delete/<int:imageId>/', UserGeneratedView.as_view(), name='user_generated_image_Delete'),
 ]
--- generated/views.py
+++ generated/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from generated.models import UserGeneratedImage
 from generated.serializers import UserGeneratedImageSerializer
 from users.models import User
 from cloths.serializers import UserImageSerializer , UserClothesImageSerializer
@@ -45,5 +46,47 @@ class UserGeneratedView(APIView):
                 return Response(serializer_generated_image.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response([serializerImage.errors, serializerClothes.errors], status=status.HTTP_400_BAD_REQUEST)
+        
+    def post(self, request, FavId):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+        try:
+            payload = jwt.decode(token,'secret',algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+        user = User.objects.filter(id=payload['id']).first()
+        # Check if the user has the specific image by ID
+        user_image = UserGeneratedImage.objects.filter(id=FavId).first()
+        if user_image:
+            if user == user_image.user:
+                user_image.is_fav=True
+                user_image.save()
+                return Response({'message': 'User image added to Fav Successfully'}, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': 'Unauthorized!'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'message': 'User does not have the specified image'}, status=status.HTTP_404_NOT_FOUND)
+        
+    def delete(self, request, imageId):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+        try:
+            payload = jwt.decode(token,'secret',algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+        user = User.objects.filter(id=payload['id']).first()
+        # Check if the user has the specific image by ID
+        user_image = UserGeneratedImage.objects.filter(id=imageId).first()
+        if user_image:
+            if user == user_image.user:
+                user_image.delete()
+                return Response({'message': 'User image deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': 'Unauthorized!'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'message': 'User does not have the specified image'}, status=status.HTTP_404_NOT_FOUND)
+        
     
     
